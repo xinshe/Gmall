f96edd9a5abd5cd1120bd2f996c55af439b20c1f
xinshe <xinshe0228@163.com>
2020-05-19 15:30:23 +0800

    项目逆向完成。

--- gmall-pms/src/main/java/com/sx/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/sx/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,12 @@
 package com.sx.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
+@MapperScan(basePackages = "com.sx.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
--- gmall-pms/src/test/java/com/sx/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/sx/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,30 @@
 package com.sx.gmall.pms;
 
+import com.sx.gmall.pms.entity.Brand;
+import com.sx.gmall.pms.entity.Product;
+import com.sx.gmall.pms.service.BrandService;
+import com.sx.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        // 测试增删改
+//        Brand brand = new Brand();
+//        brand.setName("hahaha");
+//        brandService.save(brand);
+//        System.out.println("保存成功....");
+
+        // 测试读
+        Brand byId = brandService.getById(53);
+        System.out.println("查询到...." + byId.getName());
     }
 
 }
